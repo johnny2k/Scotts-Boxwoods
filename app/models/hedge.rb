3ce96e39a8ec473f6403927a2383078a13d16b63
Jonathan Kohls <johnny2k@gmail.com>
2012-06-17 05:17:58 -0500

    ummmm

--- app/models/hedge.rb
+++ app/models/hedge.rb
@@ -1,6 +1,5 @@
 class Hedge
   attr_accessor :length, :distance, :years, :pot_size, :length_units, :distance_units
-	validates :length, :distance, :years, :pot_size, :length_units, :distance_units
 
 end
 
