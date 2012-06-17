669ffabd4c677769d4f09318882eceb34e26e667
Jonathan Kohls <johnny2k@gmail.com>
2012-06-17 14:32:50 -0500

    Precompile succeeds now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,14 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-
-$(function() {
-	    /* Convenience for forms or links that return HTML from a remote ajax call.
-			 *     The returned markup will be inserted into the element id specified.
-			 *          */
-	    $('form[ci]').live('ajax:success', function(evt, data) {
-				        var target = $(this).data('update-target');
-								        $('#' + target).html(data);
-												    });
-});
